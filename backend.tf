5c3f9723437bf37476da8b69a756c0ec7513e70f
sap-wunder <narendran0489@outlook.com>
2025-10-29 22:28:22 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Terraformstate-RG01"
-    storage_account_name = "strgaccountrfrmstate01"
+    resource_group_name  = "Terraformstate-N01"
+    storage_account_name = "strgfrtfm"
     container_name       = "tfstate"
     key                  = "tfstatefile.json"
   }
